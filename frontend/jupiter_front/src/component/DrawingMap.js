98983bf7a1024f2a29b8e34cdc410075363f1b5a
beeguriri <kkamboting@gmail.com>
2023-05-18 10:39:03 +0900

    마커 클러스터링

--- frontend/jupiter_front/src/component/DrawingMap.js
+++ frontend/jupiter_front/src/component/DrawingMap.js
@@ -45,7 +45,7 @@ const DrawingMap = () => {
         // 마커를 생성
         let Accmarker = suddenAcc.map((i) =>
             new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
+                // map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(i.GPS_Y, i.GPS_X), // 마커를 표시할 위치
                 title: "급가속", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: SuddenAccmarker // 마커 이미지 
@@ -54,7 +54,7 @@ const DrawingMap = () => {
 
         let Dropmarker = suddenDrop.map((i) =>
             new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
+                // map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(i.GPS_Y, i.GPS_X), // 마커를 표시할 위치
                 title: "급감속", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: SuddenDropmarker // 마커 이미지 
@@ -63,7 +63,7 @@ const DrawingMap = () => {
 
         let Depmarker = suddenDeparture.map((i) =>
             new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
+                // map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(i.GPS_Y, i.GPS_X), // 마커를 표시할 위치
                 title: "급출발", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: SuddenDeparturemarker // 마커 이미지 
@@ -72,13 +72,34 @@ const DrawingMap = () => {
 
         let Stopmarker = suddenStop.map((i) =>
             new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
+                // map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(i.GPS_Y, i.GPS_X), // 마커를 표시할 위치
                 title: "급정지", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: SuddenStopmarker // 마커 이미지 
             })
         );
 
+        // 마커 클러스터러 생성
+        let clusterer = new kakao.maps.MarkerClusterer({
+            map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+            averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+            minLevel: 5 // 클러스터 할 최소 지도 레벨 
+        })
+
+        clusterer.addMarkers(Accmarker)
+        clusterer.addMarkers(Dropmarker)
+        clusterer.addMarkers(Depmarker)
+        clusterer.addMarkers(Stopmarker)
+
+        // 마커 클러스터러에 클릭이벤트를 등록합니다
+        kakao.maps.event.addListener(clusterer, 'clusterclick', function(cluster) {
+
+            // 현재 지도 레벨에서 1레벨 확대한 레벨
+            var level = map.getLevel()-1;
+    
+            // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
+            map.setLevel(level, {anchor: cluster.getCenter()});
+        });
 
 
     }, []);
@@ -87,7 +108,7 @@ const DrawingMap = () => {
 
         <div id="map"
             style={{ width: "80vw", height: "80vh", border: "solid black 1px", margin: "10px 10px 10px 10px" }}>
-
+ 
         </div>
 
     );
